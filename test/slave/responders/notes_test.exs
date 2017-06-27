98fc85ada8bc194750576c2877c6c6a3b0c20457
AlexBeznos <beznosa@yahoo.com>
2017-06-28 00:26:59 +0300

    Testing. First try.

---
+++ test/slave/responders/notes_test.exs
@@ -0,0 +1,10 @@
+defmodule Slave.Responders.NotesTest do
+  use Hedwig.RobotCase
+  # TODO: Fix it
+  # @tag start_robot: true, name: "slave", responders: [{Slave.Responders.Notes, []}]
+  # test "great success - responds with a borat url", %{adapter: adapter, msg: msg} do
+  #   send adapter, {:message, %{msg | text: "note hello world"}}
+  #   assert_receive {:message, %{text: text}}
+  #   assert String.contains?(text, "Thanks.")
+  # end
+end
